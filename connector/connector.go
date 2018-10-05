810582dd084ddf39d0076bdfd9d411cd8450ee72
Lin Yang <linyang@mega.space>
2018-10-05 14:45:41 -0700

    adding .

--- connector/connector.go
+++ connector/connector.go
@@ -17,29 +17,29 @@ import (
 
 // Connector defines the shared structure for each chain-specific connector.
 type Connector interface {
-	// Name returns the name of the Connector, specific to the client implementation
+	// Name returns the name of the Connector, specific to the client implementation.
 	Name() string
 
-	// ChainName returns the name of the blockchain, eg., Bitcoin, Ethereum, Stella
+	// ChainName returns the name of the blockchain, eg., Bitcoin, Ethereum, Stella.
 	ChainName() string
 
 	// SubscribeBlock establishes blocks channel to accept new blocks.
-	// It supports "resume after" functionality by passing in the hash of the checkpoint block
+	// It supports "resume after" functionality by passing in the hash of the checkpoint block.
 	SubscribeBlock(resumeAfter *common.Hash, blocks chan<- types.Block) (Subscription, error)
 
-	// Start starts the Connector, proper setup is done here
+	// Start starts the Connector, proper setup is done here.
 	Start() error
 
-	// Stop stops the Connector and calls Unsubscribe on subscription channels
+	// Stop stops the Connector and calls Unsubscribe on subscription channels.
 	Stop() error
 
-	// QueryAccountBalance gets account balance of given address on demand
-	// It returns a float64 value in local unit
+	// QueryAccountBalance gets account balance of given address on demand.
+	// It returns a float64 value in local unit.
 	QueryAccountBalance(addr string) (<-chan float64, error)
 }
 
 // Subscription defines the shared structure for each connector's new block subscription.
 type Subscription interface {
-	// Unsubscribe is called when subscription is closed
+	// Unsubscribe is called when subscription is closed.
 	Unsubscribe()
 }
