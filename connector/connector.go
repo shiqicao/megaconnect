c62cfadce08b10d3504a5cce2bb4ab13a133a91d
Lin Yang <linyang@mega.space>
2018-10-05 14:36:52 -0700

    adding comments to Connector and Subscription interfaces

--- connector/connector.go
+++ connector/connector.go
@@ -17,15 +17,29 @@ import (
 
 // Connector defines the shared structure for each chain-specific connector.
 type Connector interface {
+	// Name returns the name of the connector, specific to the client implementation
 	Name() string
+
+	// ChainName returns the name of the blockchain, eg., Bitcoin, Ethereum, Stella
 	ChainName() string
+
+	// SubscribeBlock establishes blocks channel to accept new blocks.
+	// It supports "resume after" functionalities by passing in the hash of the checkpoint block
 	SubscribeBlock(resumeAfter *common.Hash, blocks chan<- types.Block) (Subscription, error)
+
+	// Start starts the Connector, proper setup is done here
 	Start() error
+
+	// Stop stops the Connector and call Unsubscribe on subscription channels
 	Stop() error
+
+	// QueryAccountBalance gets account balance of given address on demand
+	// It returns a float64 value in local unit
 	QueryAccountBalance(addr string) (<-chan float64, error)
 }
 
 // Subscription defines the shared structure for each connector's new block subscription.
 type Subscription interface {
+	// Unsubscribe is called when subscription is closed
 	Unsubscribe()
 }
