2e655ba89cec646018e9876c244928182e286d67
Shiqi Cao <shiqicao@gmail.com>
2018-11-07 20:50:15 -0800

    add comments

--- workflow/parser/utils/parseraction.go
+++ workflow/parser/utils/parseraction.go
@@ -18,6 +18,7 @@ import (
 	"github.com/megaspacelab/megaconnect/workflow/parser/gen/token"
 )
 
+// IntLitAction is mapped to intLit in lang.bnf
 func IntLitAction(t interface{}) (*wf.IntConst, error) {
 	lit := Lit(t)
 	i, ok := new(big.Int).SetString(lit, 10)
@@ -27,6 +28,7 @@ func IntLitAction(t interface{}) (*wf.IntConst, error) {
 	return wf.NewIntConst(i), nil
 }
 
+// BoolLitAction is mapped to boolLit in lang.bnf
 func BoolLitAction(t interface{}) (*wf.BoolConst, error) {
 	lit := Lit(t)
 	if lit == "true" {
@@ -37,6 +39,7 @@ func BoolLitAction(t interface{}) (*wf.BoolConst, error) {
 	return nil, fmt.Errorf("")
 }
 
+// StrLitAction is mapped to stringLit in lang.bnf
 func StrLitAction(t interface{}) (*wf.StrConst, error) {
 	lit := Lit(t)
 	if len(lit) < 2 {
@@ -45,6 +48,7 @@ func StrLitAction(t interface{}) (*wf.StrConst, error) {
 	return wf.NewStrConst(lit[1 : len(lit)-1]), nil
 }
 
+// MonitorAction is mapped to Monitor in lang.bnf
 func MonitorAction(
 	name interface{}, chain interface{}, expr interface{},
 	varDeclsRaw interface{}, event interface{}, eventObj interface{},
@@ -60,6 +64,7 @@ func MonitorAction(
 	return md, nil
 }
 
+// VarDeclAction is mapped to VarDecl in lang.bnf
 func VarDeclAction(nameRaw interface{}, exprRaw interface{}) (wf.IdToExpr, error) {
 	vd := wf.NewIdToExpr()
 	name := Id(nameRaw)
@@ -68,6 +73,7 @@ func VarDeclAction(nameRaw interface{}, exprRaw interface{}) (wf.IdToExpr, error
 	return vd, nil
 }
 
+// VarDeclsAction is mapped to VarDecls in lang.bnf
 func VarDeclsAction(varDeclsRaw interface{}, varDeclRaw interface{}) (wf.IdToExpr, error) {
 	varDecls := varDeclsRaw.(wf.IdToExpr)
 	if varDeclRaw == nil {
@@ -83,6 +89,7 @@ func VarDeclsAction(varDeclsRaw interface{}, varDeclRaw interface{}) (wf.IdToExp
 	return varDecls, nil
 }
 
+// ObjLitFieldsAction is mapped to ObjLitFields_ in lang.bnf
 func ObjLitFieldsAction(varDeclsRaw interface{}, varDeclRaw interface{}) (wf.IdToExpr, error) {
 	varDecls := varDeclsRaw.(wf.IdToExpr)
 	if varDeclRaw == nil {
@@ -97,6 +104,7 @@ func ObjLitFieldsAction(varDeclsRaw interface{}, varDeclRaw interface{}) (wf.IdT
 	return varDecls, nil
 }
 
+// ObjFieldsAction is mapped to ObjFields_ in lang.bnf
 func ObjFieldsAction(fieldDeclsRaw interface{}, fieldDeclRaw interface{}) (wf.IdToTy, error) {
 	fieldDecls := fieldDeclsRaw.(wf.IdToTy)
 	if fieldDeclRaw == nil {
@@ -111,21 +119,25 @@ func ObjFieldsAction(fieldDeclsRaw interface{}, fieldDeclRaw interface{}) (wf.Id
 	return fieldDecls, nil
 }
 
+// ObjAccessorAction is referenced in Expr in lang.bnf
 func ObjAccessorAction(expr interface{}, id interface{}) (*wf.ObjAccessor, error) {
 	receiver := expr.(wf.Expr)
 	accessor := Lit(id)
 	return wf.NewObjAccessor(receiver, accessor), nil
 }
 
+// ObjLitAction is mapped to ObjLit in lang.bnf
 func ObjLitAction(objFields interface{}) (*wf.ObjLit, error) {
 	fields := objFields.(wf.IdToExpr)
 	return wf.NewObjLit(fields), nil
 }
 
+// Lit converts a token to string
 func Lit(t interface{}) string {
 	return string(t.(*token.Token).Lit)
 }
 
+// Id converts a token to workflow.Id
 func Id(t interface{}) *wf.Id {
 	token := t.(*token.Token)
 	id := string(token.Lit)
